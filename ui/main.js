ecc597a510a469c2d44e4a800fd2bdccc1dbca48
ahuja2102 <36604110+ahuja2102@users.noreply.github.com>
2018-02-23 01:49:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
 console.log('Loaded!');
 alert("tadaaaaa!!!!! gotcha!!!!");
-    
+var img= document.getElementById('madi');
+  img.onclick = function() {
+      img.style.marginLeft = '100px';
+      
+      
+      
+  }
+
+
+
+
+
+
+
+
+
+
