10c2f98f7e75e42298a52a486bce79cc8d096c7e
ahuja2102 <36604110+ahuja2102@users.noreply.github.com>
2018-02-24 00:18:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function()
       }  
         
     };
-  request.open('GET', 'http://ahujaaditya7.imad.hasura-app.io/counter');
+  request.open('GET', 'http://ahujaaditya7.imad.hasura-app.io/counter', true);
   request.send(null);
 };
       
