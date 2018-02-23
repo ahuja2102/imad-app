b4695150787e7652c0dc4ca3dd5ed4a707d557aa
ahuja2102 <36604110+ahuja2102@users.noreply.github.com>
2018-02-24 00:18:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,26 @@
 console.log('Loaded!');
 var counter = 0;
 var button = document.getElementById("container");
-button.onclick = function() {
+button.onclick = function() 
+{
     
-    counter = counter+1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+      if (request.readyState == XMLHttpRequest.DONE)
+      {
+          if(request.status == 200){
+              var counter=request.responseText;
+              var span=document.getElementById("count");
+              span.innerHTML = counter.toString();
+              
+          }
+          
+      }  
+        
+    };
+  request.open('GET', 'http://ahujaaditya7.imad.hasura-app.io/counter');
+  request.send(null);
 };
       
   
